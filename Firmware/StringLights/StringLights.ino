12433ab74ac5f9bc837e2d7d51f16f6e6a24e666
Martin Placek <maplac@seznam.cz>
2019-03-10 22:59:31 +0100

    Firmware: saving/loding color variations for multi color page

--- Firmware/StringLights/StringLights.ino
+++ Firmware/StringLights/StringLights.ino
@@ -11,9 +11,10 @@
 #include <ArduinoJson.h>
 
 #define SAVED_COLORS_COUNT  30
-#define COLOR_NAME_LENGTH   12
+#define COLOR_NAME_LENGTH   13 // 12 chars + \0
 #define EFFECT_SINGLE       0
 #define EFFECT_MULTI        1
+#define MULTI_COLOR_COUNT   10
 
 //ESP AP Mode configuration
 const char *ssid = "Tenda";
@@ -36,6 +37,9 @@ char savedNames[SAVED_COLORS_COUNT][COLOR_NAME_LENGTH];
 unsigned char singleColor[3];
 unsigned char multiColor[SAVED_COLORS_COUNT][3];
 int multiColorLength;
+int multiColorCount = 10;
+int multiColorIndex;
+char multiColorNames[MULTI_COLOR_COUNT][COLOR_NAME_LENGTH];
 int currentEffect;
 int isOn;
 
@@ -147,12 +151,12 @@ void loadSettings(){
     return;
   }
   
-  if(!file.seek(7)){
+  if(!file.seek(11)){// mcbasestr='
     Serial.println("seek failed");
     file.close();
     return;
   }
-  size -= 8;
+  size -= 12;
   
   file.readBytes(buf.get(), size);
   file.close();
@@ -164,12 +168,51 @@ void loadSettings(){
     return;
   }
 
-  multiColorLength = json3["mc"].size();
+  multiColorIndex = json3["mcindex"];
+ 
+  for(int i = 0; i < json3["mcnames"].size(); ++i){
+    String newName = json3["mcnames"][i];
+    newName.toCharArray(multiColorNames[i], COLOR_NAME_LENGTH);
+  }
+
+  //---------------------------------------------------------------------
+  String fileName;
+  fileName = "/multi_color_settings_" + String(multiColorIndex) + ".js";
+  file = SPIFFS.open(fileName, "r");
+  if (!file) {
+      Serial.println("Opening multi_color_settings_x.js failed.");
+      return;
+  }
+  size = file.size();
+  if (size > 1024) {
+    Serial.println("multi_color_settings_x is too large");
+    file.close();
+    return;
+  }
+  /*
+  if(!file.seek(11)){// mcbasestr='
+    Serial.println("multi_color_settings_x seek failed");
+    file.close();
+    return;
+  }
+  size -= 12;*/
+  
+  file.readBytes(buf.get(), size);
+  file.close();
+
+  buf.get()[size] = 0;
+  JsonObject& json5 = jsonBuffer.parseObject(buf.get());
+  if (!json5.success()) {
+    Serial.println("Failed to parse multi_color_settings_x.js file");
+    return;
+  }
+  
+  multiColorLength = json5["mc"].size();
 
   for(int i = 0; i < multiColorLength; ++i){
-    multiColor[i][0] = json3["mc"][i][0];
-    multiColor[i][1] = json3["mc"][i][1];
-    multiColor[i][2] = json3["mc"][i][2];
+    multiColor[i][0] = json5["mc"][i][0];
+    multiColor[i][1] = json5["mc"][i][1];
+    multiColor[i][2] = json5["mc"][i][2];
   }
   
   //---------------------------------------------------------------------
@@ -196,7 +239,8 @@ void loadSettings(){
   }
 
   currentEffect = json4["effect"];
-  isOn = json4["isOn"];
+  isOn = 1; // todo 
+  //isOn = json4["isOn"];
 
   if(isOn){
     if(currentEffect == EFFECT_MULTI){
@@ -369,75 +413,133 @@ inline unsigned char stringToNum(char a, char b, char c){
 }
 //=============================================================================================
 void handleMultiColor(){
-  Serial.println("Handling MultioColor");
-  if(server.hasArg("type")){
-    if(server.arg("type") == "color"){
-      if(server.hasArg("r") && server.hasArg("g") && server.hasArg("b")){
-        String r = server.arg("r");
-        String g = server.arg("g");
-        String b = server.arg("b");
-        if ( (r.length() % 3) == 0 && (g.length() % 3) == 0 && (b.length() % 3) == 0){
-          if(r.length() == g.length() && g.length() == b.length()){
-            if((r.length() / 3) <= ledCount || r.length() < 0){
-              Serial.print("multi color: ");  Serial.print(r);
-              Serial.print(", ");  Serial.print(g);
-              Serial.print(", ");  Serial.println(b);
-  
-              //multiColorLength = r.length() / 3;
-              //int count = 0;
-              multiColorLength = 0;
-              for(int i = 0; i < r.length(); i += 3){
-                multiColor[multiColorLength][0] = stringToNum(r.charAt(i), r.charAt(i + 1), r.charAt(i + 2));
-                multiColor[multiColorLength][1] = stringToNum(g.charAt(i), g.charAt(i + 1), g.charAt(i + 2));
-                multiColor[multiColorLength][2] = stringToNum(b.charAt(i), b.charAt(i + 1), b.charAt(i + 2));
-                multiColorLength++;
-              }
-  
-              File file = SPIFFS.open("/multi_color_settings.js", "w");
-              if (!file) {
-                Serial.println("Opening failed.");
-                server.send(400,"text/html", "multi_color_settings.js file open failed");
-                return;
-              }
-              file.print("mcstr='{\"mc\":[");
-              for(int i = 0; i < multiColorLength; ++i){
-                if(i > 0){
-                  file.print(",");
+  Serial.println("Handling MultiColor");
+  if(server.hasArg("type") && server.hasArg("index")){
+    int index = server.arg("index").toInt();
+    if( index >= 0 || index < MULTI_COLOR_COUNT){
+      Serial.print("index: ");Serial.println(index);
+      if(server.arg("type") == "color"){
+        
+        if(server.hasArg("r") && server.hasArg("g") && server.hasArg("b")){
+          String r = server.arg("r");
+          String g = server.arg("g");
+          String b = server.arg("b");
+          if ( (r.length() % 3) == 0 && (g.length() % 3) == 0 && (b.length() % 3) == 0){
+            if(r.length() == g.length() && g.length() == b.length()){
+              if((r.length() / 3) <= ledCount || r.length() < 0){
+                Serial.print("multi color: ");  Serial.print(r);
+                Serial.print(", ");  Serial.print(g);
+                Serial.print(", ");  Serial.println(b);
+    
+                //multiColorLength = r.length() / 3;
+                //int count = 0;
+                multiColorLength = 0;
+                for(int i = 0; i < r.length(); i += 3){
+                  multiColor[multiColorLength][0] = stringToNum(r.charAt(i), r.charAt(i + 1), r.charAt(i + 2));
+                  multiColor[multiColorLength][1] = stringToNum(g.charAt(i), g.charAt(i + 1), g.charAt(i + 2));
+                  multiColor[multiColorLength][2] = stringToNum(b.charAt(i), b.charAt(i + 1), b.charAt(i + 2));
+                  multiColorLength++;
                 }
-                file.print("[");
-                file.print(multiColor[i][0]);file.print(",");
-                file.print(multiColor[i][1]);file.print(",");
-                file.print(multiColor[i][2]);file.print("]");
-              }
-              file.print("]}'");
-              file.close();
+                
+                String fileName = "/multi_color_settings_" + String(index) + ".js";
+                File file = SPIFFS.open(fileName, "w");
+                if (!file) {
+                    Serial.println("Opening failed.");
+                    server.send(400,"text/html", "multi_color_settings_x.js file open failed");
+                }else{
+                  file.print("{\"mc\":[");
+                  for(int i = 0; i < multiColorLength; ++i){
+                    if(i > 0){
+                      file.print(",");
+                    }
+                    file.print("[");
+                    file.print(multiColor[i][0]);file.print(",");
+                    file.print(multiColor[i][1]);file.print(",");
+                    file.print(multiColor[i][2]);file.print("]");
+                  }
+                  file.print("]}");
+                  file.close();
+                }
 
-              currentEffect = EFFECT_MULTI;
-              
-              for(int i = 0; i < ledCount; ++i){
-                int index = i % multiColorLength;
-                strip.SetPixelColor(i, RgbColor(multiColor[index][0], multiColor[index][1], multiColor[index][2]));
-              }
-              strip.Show();
-              
-              if(!saveCurrentSettings()){
-                server.send(400,"text/html", "current_settings.js file open failed");
-                return;
+                // if index changed
+                if(multiColorIndex != index){
+                  multiColorIndex = index;
+                  file = SPIFFS.open("/multi_color_settings.js", "w");
+                  if (!file) {
+                      Serial.println("Opening failed.");
+                      server.send(400,"text/html", "multi_color_settings.js file open failed");
+                  }else{
+                    file.print("mcbasestr='{\"mcindex\":");
+                    file.print(multiColorIndex);
+                    file.print(",\"mccount\":");
+                    file.print(MULTI_COLOR_COUNT);
+                    file.print(",\"mcnames\":[");
+                    for(int i = 0; i < MULTI_COLOR_COUNT; ++i){
+                      if(i > 0){
+                        file.print(",");
+                      }
+                      file.print("\"");
+                      file.print(String(multiColorNames[i]));
+                      file.print("\"");
+                    }
+                    file.print("]}'");
+                    file.close();
+                  }
+                }
+                currentEffect = EFFECT_MULTI;
+                
+                for(int i = 0; i < ledCount; ++i){
+                  int index = i % multiColorLength;
+                  strip.SetPixelColor(i, RgbColor(multiColor[index][0], multiColor[index][1], multiColor[index][2]));
+                }
+                strip.Show();
+                
+                if(!saveCurrentSettings()){
+                  server.send(400,"text/html", "current_settings.js file open failed");
+                  return;
+                }
+              }else{
+                server.send(400,"text/html", "r/g/b/ length is out of range");
               }
             }else{
-              server.send(400,"text/html", "r/g/b/ length is out of range");
+              server.send(400,"text/html", "r/g/b/ lengths do not match");
             }
           }else{
-            server.send(400,"text/html", "r/g/b/ lengths do not match");
+            server.send(400,"text/html", "r/g/b/ length is not divisible by 3");
           }
         }else{
-          server.send(400,"text/html", "r/g/b/ length is not divisible by 3");
+          server.send(400,"text/html", "r/g/b/ is missing");
+        }
+      }else if(server.arg("type") == "rename"){
+        String newName = server.arg("name");
+        Serial.println("new name: " + newName);
+        newName.toCharArray(multiColorNames[index], COLOR_NAME_LENGTH);
+        File file = SPIFFS.open("/multi_color_settings.js", "w");
+        if (!file) {
+            Serial.println("Opening failed.");
+            server.send(400,"text/html", "multi_color_settings.js file open failed");
+        }else{
+          file.print("mcbasestr='{\"mcindex\":");
+          file.print(multiColorIndex);
+          file.print(",\"mccount\":");
+          file.print(MULTI_COLOR_COUNT);
+          file.print(",\"mcnames\":[");
+          for(int i = 0; i < MULTI_COLOR_COUNT; ++i){
+            if(i > 0){
+              file.print(",");
+            }
+            file.print("\"");
+            file.print(String(multiColorNames[i]));
+            file.print("\"");
+          }
+          file.print("]}'");
+          file.close();
         }
       }else{
-        server.send(400,"text/html", "r/g/b/ is missing");
+        server.send(400,"text/html", "unknown type");
       }
     }else{
-      server.send(400,"text/html", "unknown type");  
+      server.send(400,"text/html", "index out of range");
     }
   }else{
     server.send(400,"text/html", "type is missing");
